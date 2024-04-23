4c325538f7aba61c982bbdd718f068dce9d47dd5
Ajay-durgesam <ajaydurgesam@gmail.com>
2024-04-23 20:31:56 +0530

    C files without opt

---
+++ K2c_files_without_optimization/WebModel.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void WebModel(k2c_tensor* dense_69_input_input, k2c_tensor* dense_71_output); 
+void WebModel_initialize(); 
+void WebModel_terminate(); 
